9823e6ee0bcffaba7aab7731df3e30ade6fbc8b6
Lucie Musilová <musil.lucie@seznam.cz>
2022-10-02 19:47:54 +0200

    oprava ukolu

--- src/components/Registration/index.jsx
+++ src/components/Registration/index.jsx
@@ -1,42 +1,90 @@
-import React, { useState } from 'react';
-import './style.css';
-import avatar from './avatar.jpg';
+import React, { useState } from "react";
+import "./style.css";
+import avatar from "./avatar.jpg";
 
 const Registration = () => {
   const [user, setUser] = useState({
-    username: '',
-    email: '',
-    password: '',
-    passwordConfirm: '',
+    username: "",
+    email: "",
+    password: "",
+    passwordConfirm: "",
   });
 
   const handleClick = (event) => {
     event.preventDefault();
     console.log(user);
     setUser({
-      username: '',
-      email: '',
-      password: '',
-      passwordConfirm: '',
+      username: "",
+      email: "",
+      password: "",
+      passwordConfirm: "",
     });
   };
-  
+
+  const handleBlur = (event) => {
+    if (user.username === "" && /\S+@\S+\.\S+/.test(event.target.value)) {
+      setUser({
+        ...user,
+        email: event.target.value,
+        username: event.target.value.split("@")[0],
+      });
+    }
+  };
+
   return (
-    <div className='registration'>
+    <div className="registration">
       <h2>Registration</h2>
       <form onSubmit={handleClick}>
         <img src={avatar} />
-        <input onChange={(event)=>{
-      setUser({ ...user, email: event.target.value, username: event.target.value.split('@')[0] })}} name="email" id="email" type="email" placeholder="Email Address" value={user.email} required/>
-        <input onChange={(event)=>{
-      setUser({ ...user, username: event.target.value })}} type="text" placeholder="User Name" value={user.username} required/>
-        <input onChange={(event)=>{
-      setUser({ ...user, password: event.target.value })}} type="text" placeholder="Password" value={user.password} required/>
-        <input onChange={(event)=>{
-      setUser({ ...user, passwordConfirm: event.target.value })}} type="text" placeholder="Confirm Password" value={user.passwordConfirm} required/>
-        <p style={{display: user.password === user.passwordConfirm ? 'none' : '' }}>The entered passwords do not match.</p>
+        <input
+          onBlur={handleBlur}
+          name="email"
+          id="email"
+          type="email"
+          placeholder="Email Address"
+          defaultValue={user.email}
+          required
+        />
+        <input
+          onChange={(event) => {
+            setUser({ ...user, username: event.target.value });
+          }}
+          type="text"
+          placeholder="User Name"
+          value={user.username}
+          required
+        />
+        <input
+          onChange={(event) => {
+            setUser({ ...user, password: event.target.value });
+          }}
+          type="password"
+          placeholder="Password"
+          value={user.password}
+          required
+        />
+        <input
+          onChange={(event) => {
+            setUser({ ...user, passwordConfirm: event.target.value });
+          }}
+          type="password"
+          placeholder="Confirm Password"
+          value={user.passwordConfirm}
+          required
+        />
+        <p
+          style={{
+            display:
+              user.passwordConfirm === "" ||
+              user.password === user.passwordConfirm
+                ? "none"
+                : "",
+          }}
+        >
+          The entered passwords do not match.
+        </p>
         <button type="submit">Register</button>
-      </form> 
+      </form>
     </div>
   );
 };
